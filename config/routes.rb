bad1db48c223dae69c16a4ac4230b02c3f1676c5
“Jessica <jmychalm13@gmail.com>
2023-09-02 11:49:36 -0500

    added user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
+
+  get "/login" => "sessions#new"
+  post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#destroy"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-ActiveRecord::Schema[7.0].define(version: 2023_07_30_182603) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_02_164200) do
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
@@ -17,6 +17,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_30_182603) do
     t.datetime "updated_at", null: false
     t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "articles"
